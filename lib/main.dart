893eb57cc33d98881f76241e849e38d9862fd451
fabiovalinhos <fabiovalinhos@gmail.com>
2020-03-29 16:51:50 -0300

    Tela do New contact

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank_persistencia/screens/contact_fom.dart';
 import 'package:bytebank_persistencia/screens/contacts_list.dart';
 import 'package:bytebank_persistencia/screens/dashboard.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class ByteBankApp extends StatelessWidget {
           textTheme: ButtonTextTheme.primary,
         )
       ),
-      home: Dashboard()
+      home: ContactForm()
     );
   }
 }
