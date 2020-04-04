9c18947575796870e90377d3eddb57f8d1175581
fabiovalinhos <fabiovalinhos@gmail.com>
2020-04-04 08:26:45 -0300

    Término do banco de dados sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:bytebank_persistencia/models/contact.dart';
 import 'package:bytebank_persistencia/screens/contact_fom.dart';
 import 'package:bytebank_persistencia/screens/contacts_list.dart';
 import 'package:bytebank_persistencia/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:bytebank_persistencia/database/app_database.dart';
 
-void main() => runApp(ByteBankApp());
+void main() {
+  runApp(ByteBankApp());
+    save(Contact(0, "Fábio Valinhos for Seven", 2000)).then((id){
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
+}
 
 class ByteBankApp extends StatelessWidget {
   // This widget is the root of your application.
